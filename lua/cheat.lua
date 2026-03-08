1d1c9195dc661e65542b61dfa8cccb479b2ad07f
theacebutler <theacebutler@gmail.com>
2026-03-08 14:40:13 -0400

    new path - initial commit

---
+++ lua/cheat.lua
@@ -0,0 +1,62 @@
+local M = {}
+-- Get the search query
+local function parse_query(q)
+	-- the first word is the programming language, the rest is the search query
+	local words = {}
+	for word in q:gmatch("%w+") do
+		table.insert(words, word)
+	end
+	local language = words[1]
+	local search_query = table.concat(words, "+", 2)
+	local sq = string.format("%s/%s", language, search_query)
+	return sq
+end
+
+local function call_api(q)
+	-- call the API and return the path to the file where the response is stored
+	local url = string.format("cheat.sh/%s", q)
+	local path = string.gsub(q, "+", "_")
+	path = string.gsub(path, "/", "_")
+	path = string.format("/tmp/cheat_%s.txt", path)
+	local cmd = string.format("curl google.com > %s", path)
+	local output = vim.fn.system(cmd)
+	if not output then
+		print("Failed to execute curl")
+		return nil
+	end
+	return path
+end
+
+local function create_buff(path)
+	local buff = vim.fn.bufnr(path, true)
+	if buff == -1 then
+		buff = vim.api.nvim_create_buf(false, true)
+	end
+	vim.bo[buff].buftype = "nofile"
+	vim.bo[buff].bufhidden = "hide"
+	vim.bo[buff].swapfile = false
+	vim.bo[buff].buflisted = false
+	vim.bo[buff].modifiable = false
+	vim.bo[buff].filetype = "markdown"
+	vim.api.nvim_buf_set_keymap(buff, "n", "q", ":q<CR>", { noremap = true, silent = true })
+	return buff
+end
+
+local function main()
+	local q = vim.fn.input("Enter programming language fallowd by the search query: ")
+	local full_query = parse_query(q)
+	local res_file = call_api(full_query)
+	if not res_file then
+		return
+	end
+
+	local buff = create_buff(res_file)
+	M.win = vim.api.nvim_open_win(buff, true, {
+		split = "below",
+		width = 50,
+		height = 30,
+		focusable = true,
+	})
+end
+main()
+return M
